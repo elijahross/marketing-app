8433f9d0aaa6133c958b75bf9e9ff5b73346a505
Elijah Ross <147133625+elijahross@users.noreply.github.com>
2023-12-05 19:30:26 +0100

    Update ContactUs.jsx

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -19,7 +19,7 @@ export const ContactUs = () => {
      <p className={`absolute ${!toggle ? "hide" : "inView, typewriter"} font-semibold sm:text-[20px] text-[16px] fornt-raleway text-white`}>{t('contact.response')}</p>
     <form className={`${layout.form} ${!toggle ? "" : "outViewForm" } w-full`} ref={form} onSubmit={handleSubmit} >
       <textarea name="message" placeholder={t('contact.form')} autoFocus={true} className={styles.textArea} />
-      <input type="email" pattern="email" title="please check the spelling of your email" name="user_email" defaultValue="..@..." className={styles.input} />
+      <input type="email" title="please check the spelling of your email" name="user_email" placeholder="..@..." className={styles.input} />
       <button type="submit" className={`sm:self-start font-raleway self-center text-[20px] py-2 px-6 glowing-btn rounded-[10px]`}>{t('contact.button')}</button>
     </form>
     </div>
